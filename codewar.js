f664eba49ff22a919eac7b56f63fef95ece55738
GudkovSergey <gudkow-sergey@mail.ru>
2024-09-12 10:35:16 +0500

    Beginner - Lost Without a Map

--- codewar.js
+++ codewar.js
@@ -16,4 +16,13 @@ function areYouPlayingBanjo(name) {
      
    }
    //-----------------------------------------------------------------------------------------------
-   
+
+/*Given an array of integers, return a new array with each value doubled.
+For example:
+[1, 2, 3] --> [2, 4, 6]*/ 
+
+function maps(x){
+    const array = x.map(el => el*2);
+    return array ; 
+    }
+
