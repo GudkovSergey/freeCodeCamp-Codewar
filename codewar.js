d164716ad49d7d1ba106803d275feca04da50734
GudkovSergey <gudkow-sergey@mail.ru>
2024-09-12 11:09:53 +0500

    [200~Century From Year

--- codewar.js
+++ codewar.js
@@ -26,3 +26,14 @@ function maps(x){
     return array ; 
     }
 
+    //-----------------------------------------------------------------------------------------------
+    /*Introduction
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.*/
+function century(year) {
+    // Finish this :)
+    return Math.floor(((year-1) /100)+1);
+  }
+
