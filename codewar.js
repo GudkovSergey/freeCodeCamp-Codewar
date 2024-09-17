81bdb94bec0f579886595fa89dec2eddb39af993
GudkovSergey <gudkow-sergey@mail.ru>
2024-09-17 16:31:19 +0500

    Disemvowel Trolls

--- codewar.js
+++ codewar.js
@@ -151,3 +151,23 @@ function squareSum(numbers){
           return result;
         }
         console.log(litres(10.2))
+        //-------------------------------------------------------------------------
+    //Disemvowel Trolls
+    /*Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.*/
+
+
+function disemvowel(str) {
+  let strArr = str.split("");
+  let result =strArr.map(el=>el.replace(/[aAeEiIoOuU]/g,''));
+  let e= result.join("");
+  return e;
+}
+console.log(disemvowel("ths wbst s fr lsrs LOL!"))
